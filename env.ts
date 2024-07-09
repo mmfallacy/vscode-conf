42be4c8bfdd4c97a95ad9c0358ba0ca944ab49a7
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-07-10 06:34:20 +0800

    chore: export Env object instead of default

--- env.ts
+++ env.ts
@@ -2,7 +2,7 @@ import { load } from "dotenv";
 
 const env = await load();
 
-export default {
+export const Env = {
   VSCODE_PROFILE_DIR: env.VSCODE_PROFILE_DIR,
   VSCODE_PROFILE_TARGET: env.VSCODE_PROFILE_TARGET,
 };
