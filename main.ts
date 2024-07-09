e44195c21f7426f6719eef570a6d9f9bb76c75cd
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-07-10 04:16:48 +0800

    feat: include extensions config to settings

--- main.ts
+++ main.ts
@@ -5,12 +5,14 @@ import { UI } from "./config/ui.ts";
 
 import { Custom } from "./config/keybinds/custom.ts";
 import { Neovim } from "./config/keybinds/neovim.ts";
+import { Config as ExtensionsConfig } from "./config/extensions/mod.ts";
 
 export const Settings = {
   ...APC,
   ...Font,
   ...Files,
   ...UI,
+  ...ExtensionsConfig,
 };
 
 export const Keybinds = [...Custom, ...Neovim];
