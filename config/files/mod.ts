8e0018856a1b24675cbf3ea6fd602860e13b012f
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-07-10 01:27:57 +0800

    feat: dynamic imports for language specific config

--- config/files/mod.ts
+++ config/files/mod.ts
@@ -1,10 +1,23 @@
+import * as path from "path";
+
 import Exclusions from "./exclusions.ts";
-import Markdown from "./markdown.ts";
 
 const Files: Record<string, unknown> = {
   "files.exclude": Exclusions,
   "files.trimTrailingWhitespace": true,
-  "[markdown]": Markdown,
 };
 
+const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
+
+for await (const dirEntry of Deno.readDir(__dirname)) {
+  const { isFile, name } = dirEntry;
+  if (!isFile) continue;
+  if (name === "mod.ts") continue;
+  if (!/\[\w+\]\.ts/.test(name)) continue;
+
+  const filename = path.basename(name, ".ts");
+  const module = await import(new URL(name, import.meta.url).toString());
+
+  Files[filename] = module.default;
+}
 export default Files;
