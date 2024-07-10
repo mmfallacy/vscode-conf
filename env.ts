dd24d619c7cf73ccff585ce3520edc565d01b63d
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-07-10 14:42:49 +0800

    chore: include env var for vscode binary

--- env.ts
+++ env.ts
@@ -5,4 +5,5 @@ const env = await load();
 export const Env = {
   VSCODE_PROFILE_DIR: env.VSCODE_PROFILE_DIR,
   VSCODE_PROFILE_TARGET: env.VSCODE_PROFILE_TARGET,
+  VSCODE_BINARY: env.VSCODE_BINARY,
 };
