2dd6db30dc9240e2a1012c17cbe922b931bc8256
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-07-11 01:43:24 +0800

    docs: include Deno.Command call to cmd rationale and TODO

--- scripts/install-ext.ts
+++ scripts/install-ext.ts
@@ -6,7 +6,9 @@ const PROFILE = Env.VSCODE_PROFILE_TARGET;
 
 let numInstalled = Packages.length;
 
+// TODO : Check OS and run command differently on Linux systems
 for (const pkg of Packages) {
+  // [Deno.run() doesn't find `.cmd` executables on `PATH`](https://github.com/denoland/deno/issues/16942)
   const cmd = new Deno.Command("cmd", {
     args: ["/c", CODE, `--profile=${PROFILE}`, "--install-extension", pkg],
   });
