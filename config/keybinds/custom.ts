c8fcee27f5a92d857b1322d288686955a0c56220
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-07-12 03:19:12 +0800

    quick exit keybinds

--- config/keybinds/custom.ts
+++ config/keybinds/custom.ts
@@ -1,3 +1,18 @@
+//Quick Exit ond Escape or Ctrl+C
+const QuickExit = [
+  {
+    command: "workbench.action.closeActiveEditor",
+    when: "inputFocus && !editorTextFocus",
+  },
+  {
+    command: "workbench.action.closeSidebar",
+    when: "listFocus",
+  },
+].flatMap((obj) => [
+  { key: "escape", type: "command", ...obj },
+  { key: "ctrl+c", type: "command", ...obj },
+]);
+
 export const Custom = [
   {
     key: "space",
@@ -9,4 +24,5 @@ export const Custom = [
     key: "ctrl+space",
     command: "whichkey.show",
   },
+  ...QuickExit,
 ];
