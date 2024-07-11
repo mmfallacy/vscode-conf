7065373ffe2e7d155cc2e4423d84b97a81f9d162
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-07-12 01:48:17 +0800

    fix: apc extension not included in package list

--- config/extensions/mod.ts
+++ config/extensions/mod.ts
@@ -1,7 +1,11 @@
 import { VSCodeNeovim } from "./vscode-neovim/mod.ts";
 import { WhichKey } from "./which-key/mod.ts";
 
-export const Packages = ["VSpaceCode.whichkey", "asvetliakov.vscode-neovim"];
+export const Packages = [
+  "VSpaceCode.whichkey",
+  "asvetliakov.vscode-neovim",
+  "drcika.apc-extension",
+];
 
 export const Config = {
   ...WhichKey,
